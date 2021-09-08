2aede2f14be72fb098a8b0c3bbd47a1b62c92326
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-09-08 17:15:35 -0300

    chore: final product page

--- template/js/index.js
+++ template/js/index.js
@@ -1,2 +1,4 @@
 import '#template/js/'
 import './custom-js/pages'
+
+storefront.on('widget:@ecomplus/widget-minicart', function () {setTimeout(function () {document.getElementById('product-payment').classList.add('show')}, 1000);})
